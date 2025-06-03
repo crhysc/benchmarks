444804a17df21f4569f025a78351aa56283a52b1
Rhys Campbell <crhysc@Janes-MacBook-Pro.local>
2025-06-03 10:52:51 -0400

    get python script drafted

--- tc_supercon/run_id_prop.sh
+++ tc_supercon/run_id_prop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 mkdir agpt_tc_supercon
-pip install data_reqs.txt
+pip install -r data_reqs.txt
 python id_prop.py \
 	--dataset dft_3d \
         --id-key jid \
+	--output agpt_tc_supercon
         --target Tc_supercon \
         --seed 123 \
         --max-size 1000
