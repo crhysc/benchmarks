f7b85b5a776da30c24a2c97208a2af857a1d6af6
Rhys Campbell <crhysc@Janes-MacBook-Pro.local>
2025-06-03 11:01:25 -0400

    data pipeline worked as intended

--- tc_supercon/run_id_prop.sh
+++ tc_supercon/run_id_prop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-mkdir agpt_tc_supercon
-pip install -r data_reqs.txt
+mkdir -p agpt_tc_supercon
+uv pip install jarvis-tools pymatgen numpy pandas tqdm
 python id_prop.py \
-	--dataset dft_3d \
-        --id-key jid \
-	--output agpt_tc_supercon
-        --target Tc_supercon \
-        --seed 123 \
-        --max-size 1000
+    --dataset dft_3d \
+    --id-key jid \
+    --output agpt_tc_supercon \
+    --target Tc_supercon \
+    --seed 123 \
+    --max-size 1000
