1a25c0064fbbb61f463c3f82a6a274f7a6680394
Campbell <crc00042@mix.wvu.edu>
2025-06-04 08:27:16 -0600

    syntax

--- tc_supercon/run_cdvae_data.sh
+++ tc_supercon/run_cdvae_data.sh
@@ -10,12 +10,12 @@ path = "../models/cdvae/data/supercon"
 files = ["train.csv", "test.csv", "val.csv"]
 for file in files:
 	file_path = os.path.join(path,file)
-	if os.path.exists(file_path)
+	if os.path.exists(file_path):
 		os.remove(file_path)
 	os.rename(file, file_path)
 yaml_path = "../models/cdvae/conf/data/supercon.yaml"
 url = "https://github.com/crhysc/utilities/blob/main/supercon.yaml"
-if not os.path.exists(yaml_path)
+if not os.path.exists(yaml_path):
 	urllib.request.urlretrieve(url, yaml_path)
 PYCODE
 
