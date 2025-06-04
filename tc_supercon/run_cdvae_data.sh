949d8a4aa5df7eb88cdcba87d42920d54f343620
Campbell <crc00042@mix.wvu.edu>
2025-06-04 08:56:41 -0600

    wget to get supercon.yaml

--- tc_supercon/run_cdvae_data.sh
+++ tc_supercon/run_cdvae_data.sh
@@ -3,9 +3,9 @@ set -e
 mkdir -p ../models/cdvae/data/supercon/
 uv pip install jarvis-tools pymatgen numpy pandas tqdm
 python generate_data_cdvae.py
+wget "https://github.com/crhysc/utilities/blob/main/supercon.yaml"
 python - <<'PYCODE'
 import os
-import urllib.request
 path = "../models/cdvae/data/supercon"
 files = ["train.csv", "test.csv", "val.csv"]
 for file in files:
@@ -14,8 +14,9 @@ for file in files:
 		os.remove(file_path)
 	os.rename(file, file_path)
 yaml_path = "../models/cdvae/conf/data/supercon.yaml"
-url = "https://github.com/crhysc/utilities/blob/main/supercon.yaml"
 if not os.path.exists(yaml_path):
-	urllib.request.urlretrieve(url, yaml_path)
+	os.rename("supercon.yaml", yaml_path)
+else:
+	os.remove("supercon.yaml")
 PYCODE
 
