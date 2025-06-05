98d850aff4da71156d5ecebf70b5c1e3b891834d
Campbell <crc00042@mix.wvu.edu>
2025-06-05 13:18:32 -0600

    cdvae working

--- job_runs/cdvae_benchmark/create_conda_env.sh
+++ job_runs/cdvae_benchmark/create_conda_env.sh
@@ -3,12 +3,12 @@
 cd /lab/mml/kipp/677/jarvis/rhys/benchmarks/models/cdvae
 
 mamba env create -f env.yml -y
-mamba activate cdvae
+source /home/crc8/miniconda3/etc/profile.d/conda.sh
+conda activate cdvae
 mamba install -c conda-forge "torchmetrics<0.8" --yes
 mamba install mkl=2024.0 --yes
-pip install uv
-uv pip install "monty==2022.9.9"
+pip install "monty==2022.9.9"
 mamba install -c conda-forge "pymatgen>=2022.0.8,<2023" --yes
-uv pip install pandas jarvis-tools
-uv pip install --upgrade torch_geometric==1.7.0
-uv pip install -e .
+pip install pandas jarvis-tools
+pip install --upgrade torch_geometric==1.7.0
+pip install -e .
