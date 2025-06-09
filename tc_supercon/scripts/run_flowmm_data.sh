b9c5b008ba3cab6ebd18733e1d34c0bd59f3325f
Rhys Campbell <crhysc@Janes-MacBook-Pro.local>
2025-06-09 13:58:04 -0400

    create pipeline prototype

--- tc_supercon/scripts/run_flowmm_data.sh
+++ tc_supercon/scripts/run_flowmm_data.sh
@@ -1,62 +1,40 @@
-# data downloader and preprocessor
-!wget -q https://raw.githubusercontent.com/crhysc/utilities/refs/heads/main/supercon_preprocess.py
-
+#!/bin/bash
 #run data preprocessor
-%cd /content/flowmm
-!conda run -p /usr/local/envs/flowmm_env --live-stream \
-    python supercon_preprocess.py \
+python scripts/supercon_preprocess.py \
         --dataset dft_3d \
         --id-key jid \
         --target Tc_supercon \
         --train-ratio 0.8 --val-ratio 0.1 --test-ratio 0.1 \
         --seed 123 \
         --max-size 25
-print("Done")
 
-# mv everything to the correct spot
-%cd /content
-%mkdir /content/flowmm/data/supercon
-%mv /content/flowmm/train.csv /content/flowmm/data/supercon/
-%mv /content/flowmm/val.csv /content/flowmm/data/supercon/
-%mv /content/flowmm/test.csv /content/flowmm/data/supercon/
-print("Done")
+# move everything to the right spot
+mkdir ../models/flowmm/data/supercon
+mv ./train.csv ../models/flowmm/data/supercon/
+mv ./val.csv ../models/flowmm/data/supercon/
+mv ./test.csv ../models/flowmm/data/supercon/
 
 # pull hydra config yaml
-%cd /content/flowmm/scripts_model/conf/data/
-!wget https://raw.githubusercontent.com/crhysc/utilities/refs/heads/main/supercon.yaml
-%cat supercon.yaml
+cd ../models/flowmm/scripts_model/conf/data/
+wget https://raw.githubusercontent.com/crhysc/utilities/refs/heads/main/supercon.yaml
 
-# generate necessary yaml files for training
-%rm /content/flowmm/src/flowmm/rfm/manifolds/atom_density.yaml
-%rm /content/flowmm/src/flowmm/rfm/manifolds/spd_pLTL_stats.yaml
-%rm /content/flowmm/src/flowmm/rfm/manifolds/spd_std_coef.yaml
+# remove yamls that might exist from past pipeline runs
+rm /lab/mml/kipp/677/jarvis/rhys/benchmarks/models/flowmm/src/flowmm/model/stats_supercon*
+python - <<'PYCODE'
+import os
+files = ["atom_density.yaml", "spd_pLTL_stats.yaml", "spd_std_coef.yaml", "lattice_params_stats.yaml"]
+path = "/lab/mml/kipp/677/jarvis/rhys/benchmarks/models/flowmm/src/flowmm/rfm/manifolds"
+for file in files:
+	filepath = os.path.join(file,path)
+	if os.path.exists(filepath):
+		os.remove(filepath)
+PYCODE
 
-%cd /content/flowmm
-!bash create_env_file.sh && \
- echo "successfully ran create_env_file.sh" && \
- HYDRA_FULL_ERROR=1 \
- FLOWMM_DEBUG=1 \
- conda run -p /usr/local/envs/flowmm_env --live-stream \
-    python -u -m flowmm.rfm.manifolds.spd
-
-# generate lattice_params_stats.yaml
-!rm /content/flowmm/src/flowmm/rfm/manifolds/lattice_params_stats.yaml
-%cd /content/flowmm
-!bash create_env_file.sh && \
- echo "successfully ran create_env_file.sh" && \
- HYDRA_FULL_ERROR=1 \
- conda run -p /usr/local/envs/flowmm_env --live-stream \
-    python -u -m flowmm.rfm.manifolds.lattice_params
-
-# create affine stats yaml
-%rm /content/flowmm/src/flowmm/model/stats_supercon*
-%cd /content/flowmm
-!bash create_env_file.sh && \
- echo "successfully ran create_env_file.sh" && \
- HYDRA_FULL_ERROR=1 \
- conda run -p /usr/local/envs/flowmm_env --live-stream \
-    python -u -m flowmm.model.standardize \
-                 data=supercon
+# create necessary yamls for training and inference
+cd /lab/mml/kipp/677/jarvis/rhys/benchmarks/models/flowmm 
+python -u -m flowmm.rfm.manifolds.spd
+python -u -m flowmm.rfm.manifolds.lattice_params
+python -u -m flowmm.model.standardize data=supercon
 
 
 
