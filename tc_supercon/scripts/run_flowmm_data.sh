11988236d55decadd11707a040ea9f5a0db2228f
Campbell <crc00042@mix.wvu.edu>
2025-06-10 08:28:31 -0600

    remove yaml making due to the need for the flowmm conda env

--- tc_supercon/scripts/run_flowmm_data.sh
+++ tc_supercon/scripts/run_flowmm_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#run data preprocessor
+# run data preprocessor
 python scripts/supercon_preprocess.py \
         --dataset dft_3d \
         --id-key jid \
@@ -18,23 +18,6 @@ mv ./test.csv ../models/flowmm/data/supercon/
 cd ../models/flowmm/scripts_model/conf/data/
 wget https://raw.githubusercontent.com/crhysc/utilities/refs/heads/main/supercon.yaml
 
-# remove yamls that might exist from past pipeline runs
-rm /lab/mml/kipp/677/jarvis/rhys/benchmarks/models/flowmm/src/flowmm/model/stats_supercon*
-python - <<'PYCODE'
-import os
-files = ["atom_density.yaml", "spd_pLTL_stats.yaml", "spd_std_coef.yaml", "lattice_params_stats.yaml"]
-path = "/lab/mml/kipp/677/jarvis/rhys/benchmarks/models/flowmm/src/flowmm/rfm/manifolds"
-for file in files:
-	filepath = os.path.join(file,path)
-	if os.path.exists(filepath):
-		os.remove(filepath)
-PYCODE
-
-# create necessary yamls for training and inference
-cd /lab/mml/kipp/677/jarvis/rhys/benchmarks/models/flowmm 
-python -u -m flowmm.rfm.manifolds.spd
-python -u -m flowmm.rfm.manifolds.lattice_params
-python -u -m flowmm.model.standardize data=supercon
 
 
 
