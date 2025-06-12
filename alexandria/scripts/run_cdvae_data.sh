b7bee069753a9c6da1a18ab0dd3ace0f6372b4c1
Rhys Campbell <crhysc@Janes-MBP.lan>
2025-06-12 13:59:09 -0400

    switch to alexandria

--- alexandria/scripts/run_cdvae_data.sh
+++ alexandria/scripts/run_cdvae_data.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
-mkdir -p ../models/cdvae/data/supercon/
+mkdir -p ../models/cdvae/data/alexandria/
 uv pip install jarvis-tools pymatgen numpy pandas tqdm
 python scripts/generate_data_cdvae.py
 python - <<'PYCODE'
 import os
-path = "../models/cdvae/data/supercon"
+path = "../models/cdvae/data/alexandria"
 files = ["train.csv", "test.csv", "val.csv"]
 for file in files:
 	file_path = os.path.join(path,file)
 	if os.path.exists(file_path):
 		os.remove(file_path)
 	os.rename(file, file_path)
-yaml_path = "../models/cdvae/conf/data/supercon.yaml"
 PYCODE
 
